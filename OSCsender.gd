43f5ed6ebc0dd50c7f9d7dee400fba3361487328
frankiezafe <frankie@frankiezafe.org>
2018-10-28 14:50:16 +0100

    updated demo

--- OSCsender.gd
+++ OSCsender.gd
@@ -5,9 +5,9 @@ var parent = null
 func _process(delta):
 	if (parent == null):
 		return
-	msg_address("/emitter/rot" )
-	msg_add_quat( Quat(parent.global_transform.basis) )
-	msg_send()
+	#msg_address("/emitter/rot" )
+	#msg_add_quat( Quat(parent.global_transform.basis) )
+	#msg_send()
 	
 	msg_address("/emitter/pos" )
 	msg_add_v3( parent.global_transform.origin )
